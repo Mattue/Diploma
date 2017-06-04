bf0d6ccc7ef5b7fb2b90c920c7cdfb95342b3995
Mattue <mexanik.o@gmail.com>
2017-06-04 21:04:21 +0600

    команды работы с сервой

---
+++ Arduino/firmvare v2/src/Mover.cpp
@@ -0,0 +1,97 @@
+#include "Mover.h"
+
+#define IN3 5
+#define IN4 4
+#define ENB 3
+#define SERVOPIN 9
+#define TURN 20
+#define LEFT -1
+#define STRAIGHT 0
+#define RIGHT 1
+
+Mover::Mover()
+{
+  straightValue = 90;
+  wheelPower = 150;
+
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  TurnServo.attach(SERVOPIN);
+  TurnServo.write(straightValue);
+}
+
+Mover::~Mover()
+{
+}
+
+int Mover::execCommand(int commandID, String commandName)
+{
+  switch(commandID)
+  {
+    case 111:
+      return turnWheels(STRAIGHT);
+    case 112:
+      return turnWheels(LEFT);
+    case 113:
+      return turnWheels(RIGHT);
+    case 114:
+      return changeStraighValue(commandName);
+    default:
+      return 1;
+  }
+}
+
+int Mover::getNumValue(String commandName)
+{
+  String commandValue;
+  int i = commandName.length();
+
+  while (commandValue[i] != '_')
+  {
+    commandValue += commandName[i];
+    i--;
+  }
+
+  if(commandValue.length() <= 1)
+  return commandValue.toInt();
+
+  String revValue;
+
+  for(int i = commandValue.length(); i != 0; i--)
+  {
+    revValue += commandValue[i];
+  }
+  return revValue.toInt();
+}
+
+int Mover::turnWheels(int turn)
+{
+  switch (turn)
+  {
+    case -1:
+      TurnServo.write(straightValue - TURN);
+      return 0;
+    case 0:
+      TurnServo.write(straightValue);
+      return 0;
+    case 1:
+      TurnServo.write(straightValue + TURN);
+      return 0;
+    default:
+      return 1;
+  }
+}
+
+int Mover::turnWheelsByDegree(int degree)
+{
+  TurnServo.write(straightValue + degree);
+  return 0;
+}
+
+int Mover::changeStraighValue(String commandName)
+{
+    straightValue = getNumValue(commandName);
+    return 0;
+}
--- Arduino/firmvare v2/src/Mover.h
+++ Arduino/firmvare v2/src/Mover.h
@@ -1,10 +1,20 @@
 #include <Arduino.h>
+#include <Servo.h>
 
 class Mover
 {
   private:
     Mover();
     ~Mover();
+    Servo TurnServo;
+    int straightValue;
+    int wheelPower;
+    int execCommand(int commandID, String commandName);
+    int getNumValue(String a);
 
   public:
-}
+    int turnWheels(int turn);
+    int turnWheelsByDegree(int degree);
+    int changeStraighValue(String commandName);
+    int startWheels();
+};
